b3d31ad453a8374124dc6dbb26bd82acd52d1b18
Vishal-8155 <69633165+gokuviku@users.noreply.github.com>
2024-06-26 14:25:15 +0530

    filtering

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -14,6 +14,13 @@ const Shop = () => {
 
     const dispatch = useDispatch()
 
+    const categoriesQuery = useFetchCategoriesQuery()
+
+    const filteredProductsQuery = useGetFilteredProductsQuery({
+        checked,
+        radio,
+    })
+
     const { categories, products, checked, radio } = useSelector((state) => state.shop)
 
     useEffect(() => {
@@ -36,17 +43,83 @@ const Shop = () => {
                 dispatch(setProducts(filteredProducts))
             }
         }
-    })
+    }, [check, radio, filteredProductsQuery.data, dispatch, priceFilter])
 
-    const categoriesQuery = useFetchCategoriesQuery()
 
-    const filteredProductsQuery = useGetFilteredProductsQuery({
-        checked,
-        radio,
-    })
+    const handleBrandClick = (brand) => {
+        const productsByBrand = filteredProductsQuery.data?.filter(
+            (product) => product.brand === brand);
+        dispatch(setProducts(productsByBrand));
+    }
+
+    const handleCheck = (value, id) => {
+        const updatedChecked = value ? [...checked, id] : checked.filter((c) => c !== id)
+        dispatch(setChecked(updatedChecked))
+    }
+
+    //add all brand options to unique brand
+
+    const uniqueBrand = [
+        ...Array.from(
+            new Set(filteredProductsQuery.data
+                ?.map((product) => product.brand)
+                .filter((brand) => brand !== undefined)
+            )
+        )
+    ]
+
+    const handlePriceChange = e => {
+        setPriceFilter(e.target.value)
+    }
 
     return (
-        <div>Shop</div>
+        <>
+            <div className="container mx-auto">
+                <div className="fixed md:flex-row">
+                    <div className="bg-[#151515] p-3 mt-2 mb-2">
+                        <h2 className="h4 text-center py-2 bg-black rounded-full mb-2">
+                            Filter By Categories
+                        </h2>
+                        <div className="p-5 w-[15rem]">
+                            {categories?.map((c) => (
+                                <div key={c._id} className="mb-2">
+                                    <div className="flex items-center mr-4">
+                                        <input
+                                            type="checkbox"
+                                            id="redCheckbox"
+                                            onChange={(e) => handleCheck(e.target.value)}
+                                            className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                        />
+
+                                        <label htmlFor="pink-checkbox" className="ml-2 text-sm font-medium text-white dark:text-gray-300" >{c.name}</label>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        <h2 className="h4 text center py-2 bg-black rounded-full mb-2">Filter By Brands</h2>
+                        <div className="p-5">
+                            {uniqueBrand?.map((brand) => (
+                                <>
+                                    <div className="flex items-center mr-4 mb-5">
+                                        <input type="radio" name="brand" id={brand}
+                                            onChange={() => handleBrandClick(brand)}
+                                            className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                        />
+                                        <label htmlFor="pink-radio" className="ml-2 text-sm font-medium text-white dark:text-gray-300">{brand}</label>
+                                    </div>
+                                </>
+                            ))}
+                        </div>
+
+                        <h2 className="h4 text-center py-2 bg-black rounded-full mb-2">
+                            Filter By Price
+                        </h2>
+
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 export default Shop
